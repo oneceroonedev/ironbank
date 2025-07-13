a94e005f1b57ee1eec2adb572634d5d49c02d8c0
Sonia <oneceroonedev@gmail.com>
2025-07-14 01:01:55 +0200

    Add Spring Boot base

---
+++ src/test/java/com/ironbank/bankingsystem/IronBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironbank.bankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
